88c159db8a715c452b26b7ec9986b82d7f3e9242
saskia wohlers <saskia.wohlers@fh-erfurt.de>
2022-01-13 17:15:10 +0100

    Animationen

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -58,10 +58,22 @@ public class ThirdPersonMovement : MonoBehaviour
             float targetAngle = Mathf.Atan2(playerMovementInput.x, playerMovementInput.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
 
+            if(Physics.CheckSphere(groundCheck.position, groundDistance, groundMask))
+            {
+                animator.SetBool("isRunning", true);
+                animator.SetBool("isIdle", false);
+            }
+
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 MoveVector = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
             playerRb.MovePosition(transform.position + MoveVector.normalized * speed * Time.deltaTime);
+        }
+
+        else
+        {
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isIdle", true);
         }
 
         //----------------Jumping----------------------------------
@@ -74,11 +86,35 @@ public class ThirdPersonMovement : MonoBehaviour
                 animator.SetBool("isIdle", false);
                 playerAudio.PlayOneShot(jumpSound, 0.7f);
             }
+
         }
+
         else
         {
             animator.SetBool("isJumping", false);
             animator.SetBool("isIdle", true);
+        }
+
+        //----------------Falling----------------------------------
+       if (playerRb.velocity.y < -0.5f)
+        {
+            animator.SetBool("isFalling", true);
+            animator.SetBool("isIdle", false);
+        }
+        else
+        {
+            animator.SetBool("isIdle", true);
+            animator.SetBool("isFalling", false);
+        }
+    }
+
+    //----------------Enenmy throwback----------------------------------
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Vector3 dir = collision.transform.position - transform.position;
+            playerRb.AddForce(dir * 100f);
         }
     }
 
